3d4cc5f572a5fadda612bf60a15129a68813c1d9
Mark Edward Fuller <markedwardf@mobileye.com>
2023-08-22 10:44:54 +0300

    rewrite using stdlib only

--- wicp.go
+++ wicp.go
@@ -2,33 +2,100 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"time"
-
-	"github.com/aws/aws-sdk-go-v2/config"
 )
 
+type Credentials struct {
+	AccessKeyId     string `json:"AccessKeyId"`
+	SecretAccessKey string `json:"SecretAccessKey"`
+	SessionToken    string `json:"SessionToken"`
+}
+
+type AssumeRoleWithWebIdentityResult struct {
+	Creds Credentials `json:"Credentials"`
+}
+
+type AssumeRoleWithWebIdentityResponse struct {
+	Result AssumeRoleWithWebIdentityResult `json:"AssumeRoleWithWebIdentityResult"`
+}
+
+type Response struct {
+	Response AssumeRoleWithWebIdentityResponse `json:"AssumeRoleWithWebIdentityResponse"`
+}
+
 func main() {
-	if _, present := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FIKLE"); !present {
+	if _, ok := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FILE"); !ok {
 		log.Fatal("AWS_WEB_IDENTITY_TOKEN_FILE not set")
 	}
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+	defaultHttpClient := &http.Client{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	/*
+		"wget -q -O- --header \"Accept: application/json\" \"https://sts.$${AWS_REGION}.amazonaws.com/?Action=AssumeRoleWithWebIdentity&RoleArn=$${AWS_ROLE_ARN}&WebIdentityToken=$${AWS_WEB_IDENTITY_TOKEN_FILE}&RoleSessionName=app1&Version=2011-06-15\" | jq .AssumeRoleWithWebIdentityResponse.AssumeRoleWithWebIdentityResult.Credentials
+	*/
+
+	awsRegion, ok := os.LookupEnv("AWS_REGION")
+	if !ok {
+		log.Fatal("AWS_REGION not set")
+	}
+
+	baseURL := fmt.Sprintf("https://sts.%s.amazonaws.com", awsRegion)
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		baseURL,
+		http.NoBody,
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	crd, err := cfg.Credentials.Retrieve(context.TODO())
+
+	awsRoleArn, ok := os.LookupEnv("AWS_ROLE_ARN")
+	if !ok {
+		log.Fatal("AWS_ROLE_ARN not set")
+	}
+
+	awsWebIdentityToken, ok := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FILE")
+	if !ok {
+		log.Fatal("AWS_WEB_IDENTITY_TOKEN_FILE not set")
+	}
+
+	q := url.Values{}
+	q.Add("api_key", "key_from_environment_or_flag")
+	q.Add("RoleArn", awsRoleArn)
+	q.Add("WebIdentityToken", awsWebIdentityToken)
+	q.Add("RoleSessionName", "app1")
+	q.Add("Version", "2011-06-15")
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := defaultHttpClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer resp.Body.Close()
+
+	var awsResp Response
+	if err := json.NewDecoder(resp.Body).Decode(&awsResp); err != nil {
+		log.Printf("Could not decode JSON Response: %s", err)
+	}
+
+	crd := awsResp.Response.Result.Creds
+
 	fmt.Printf("{ \"Version\": 1, "+
 		"\"AccessKeyId\": \"%s\", "+
 		"\"SecretAccessKey\": \"%s\", "+
-		"\"SessionToken\": \"%s\", "+
-		"\"Expiration\": \"%s\" }",
-		crd.AccessKeyID,
+		"\"SessionToken\": \"%s\", ",
+		crd.AccessKeyId,
 		crd.SecretAccessKey,
-		crd.SessionToken,
-		crd.Expires.Format(time.RFC3339))
+		crd.SessionToken)
 }
